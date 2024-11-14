355c353e4a692ac6c4cacef99629de91b3f8358e
SerhatCanBakir <serhat2534serhat@outlook.com>
2024-11-14 18:02:56 +0300

    Proje dosyası ekle.

---
+++ MultiMedia2/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace MultiMedia2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.Button();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.Save = new System.Windows.Forms.Button();
+            this.UndoBtn = new System.Windows.Forms.Button();
+            this.RedoBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(36, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1297, 695);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 740);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1444, 32);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(179, 25);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(36, 8);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(189, 8);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Location = new System.Drawing.Point(1333, 72);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(88, 37);
+            this.ClearBtn.TabIndex = 4;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(1333, 172);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(88, 37);
+            this.Load.TabIndex = 5;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Location = new System.Drawing.Point(343, 4);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(75, 34);
+            this.buttonColor.TabIndex = 6;
+            this.buttonColor.Text = "COLOR";
+            this.buttonColor.UseVisualStyleBackColor = true;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click_1);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(1333, 132);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(88, 34);
+            this.Save.TabIndex = 7;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // UndoBtn
+            // 
+            this.UndoBtn.Location = new System.Drawing.Point(1164, 8);
+            this.UndoBtn.Name = "UndoBtn";
+            this.UndoBtn.Size = new System.Drawing.Size(75, 35);
+            this.UndoBtn.TabIndex = 8;
+            this.UndoBtn.Text = "Undo";
+            this.UndoBtn.UseVisualStyleBackColor = true;
+            this.UndoBtn.Click += new System.EventHandler(this.UndoBtn_Click);
+            // 
+            // RedoBtn
+            // 
+            this.RedoBtn.Location = new System.Drawing.Point(1245, 8);
+            this.RedoBtn.Name = "RedoBtn";
+            this.RedoBtn.Size = new System.Drawing.Size(75, 35);
+            this.RedoBtn.TabIndex = 9;
+            this.RedoBtn.Text = "Redo";
+            this.RedoBtn.UseVisualStyleBackColor = true;
+            this.RedoBtn.Click += new System.EventHandler(this.RedoBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1444, 772);
+            this.Controls.Add(this.RedoBtn);
+            this.Controls.Add(this.UndoBtn);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.buttonColor);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button UndoBtn;
+        private System.Windows.Forms.Button RedoBtn;
+    }
+}
+
---
+++ MultiMedia2/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MultiMedia2
+{
+    public partial class Form1 : Form
+    {
+        private Point startPoint;
+        private Point endPoint;
+        private bool isDrawing = false;
+        private Bitmap canvasBitmap; // Bitmap to store the drawings
+        private Pen pen;
+        private float thickness = 2; // Default thickness
+        private string selectedShape = "Line"; // Default shape
+        private Stack<Bitmap> undoStack = new Stack<Bitmap>();
+        private Stack<Bitmap> redoStack = new Stack<Bitmap>();
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize pen and canvas bitmap
+            pen = new Pen(Color.Blue, thickness);
+            canvasBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            // Set up the ComboBox for shape selection
+            comboBox1.Items.AddRange(new object[] { "Line", "Square", "Rectangle", "Circle", "Ellipse", "Arc", "Pie", "Polygon", "Bezier Curve" });
+            comboBox1.SelectedIndex = 0; // Default to "Line"
+
+            // Set pictureBox1 image to the canvas bitmap
+            pictureBox1.Image = canvasBitmap;
+
+            // Wire up pictureBox events
+            pictureBox1.MouseDown += PictureBox1_MouseDown;
+            pictureBox1.MouseMove += PictureBox1_MouseMove;
+            pictureBox1.MouseUp += PictureBox1_MouseUp;
+            pictureBox1.Paint += PictureBox1_Paint;
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Start drawing
+            SaveState();
+            isDrawing = true;
+            startPoint = e.Location;
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Track mouse movement while drawing
+            if (isDrawing)
+            {
+                endPoint = e.Location;
+                pictureBox1.Invalidate(); // Trigger paint event to show temporary shape
+            }
+
+            // Update status bar information
+            toolStripStatusLabel1.Text = $"Tool: {selectedShape}, Thickness: {thickness}, Color: {pen.Color.Name}, Coordinates: ({e.X}, {e.Y})";
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Complete the drawing and save it to the bitmap
+            if (isDrawing)
+            {
+                isDrawing = false;
+
+                using (Graphics g = Graphics.FromImage(canvasBitmap))
+                {
+                    switch (selectedShape)
+                    {
+                        case "Line":
+                            g.DrawLine(pen, startPoint, endPoint);
+                            break;
+                        case "Square":
+                            DrawSquare(g);
+                            break;
+                        case "Rectangle":
+                            DrawRectangle(g);
+                            break;
+                        case "Circle":
+                            DrawCircle(g);
+                            break;
+                        case "Ellipse":
+                            DrawEllipse(g);
+                            break;
+                        case "Arc":
+                            DrawArc(g);
+                            break;
+                        case "Pie":
+                            DrawPie(g);
+                            break;
+                        case "Polygon":
+                            DrawPolygon(g);
+                            break;
+                        case "Bezier Curve":
+                            DrawBezierCurve(g);
+                            break;
+                    }
+                }
+
+                pictureBox1.Invalidate(); // Refresh the pictureBox to show the updated drawing
+            }
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Only show the temporary drawing during mouse movement
+            if (isDrawing)
+            {
+                switch (selectedShape)
+                {
+                    case "Line":
+                        e.Graphics.DrawLine(pen, startPoint, endPoint);
+                        break;
+                    case "Square":
+                        DrawSquare(e.Graphics);
+                        break;
+                    case "Rectangle":
+                        DrawRectangle(e.Graphics);
+                        break;
+                    case "Circle":
+                        DrawCircle(e.Graphics);
+                        break;
+                    case "Ellipse":
+                        DrawEllipse(e.Graphics);
+                        break;
+                    case "Arc":
+                        DrawArc(e.Graphics);
+                        break;
+                    case "Pie":
+                        DrawPie(e.Graphics);
+                        break;
+                    case "Polygon":
+                        DrawPolygon(e.Graphics);
+                        break;
+                    case "Bezier Curve":
+                        DrawBezierCurve(e.Graphics);
+                        break;
+                }
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            // Update pen thickness
+            thickness = (float)numericUpDown1.Value;
+            pen.Width = thickness;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Update the selected shape based on ComboBox selection
+            selectedShape = comboBox1.SelectedItem.ToString();
+        }
+
+
+        private void DrawSquare(Graphics g)
+        {
+            int sideLength = Math.Min(Math.Abs(endPoint.X - startPoint.X), Math.Abs(endPoint.Y - startPoint.Y));
+            int x = Math.Min(startPoint.X, endPoint.X);
+            int y = Math.Min(startPoint.Y, endPoint.Y);
+            g.DrawRectangle(pen, x, y, sideLength, sideLength);
+        }
+
+        private void DrawRectangle(Graphics g)
+        {
+            int x = Math.Min(startPoint.X, endPoint.X);
+            int y = Math.Min(startPoint.Y, endPoint.Y);
+            int width = Math.Abs(endPoint.X - startPoint.X);
+            int height = Math.Abs(endPoint.Y - startPoint.Y);
+            g.DrawRectangle(pen, x, y, width, height);
+        }
+
+        private void DrawCircle(Graphics g)
+        {
+            int diameter = Math.Min(Math.Abs(endPoint.X - startPoint.X), Math.Abs(endPoint.Y - startPoint.Y));
+            int x = Math.Min(startPoint.X, endPoint.X);
+            int y = Math.Min(startPoint.Y, endPoint.Y);
+            g.DrawEllipse(pen, x, y, diameter, diameter);
+        }
+
+        private void DrawEllipse(Graphics g)
+        {
+            int width = Math.Abs(endPoint.X - startPoint.X);
+            int height = Math.Abs(endPoint.Y - startPoint.Y);
+            g.DrawEllipse(pen, startPoint.X, startPoint.Y, width, height);
+        }
+
+        private void DrawArc(Graphics g)
+        {
+            int width = Math.Abs(endPoint.X - startPoint.X);
+            int height = Math.Abs(endPoint.Y - startPoint.Y);
+            g.DrawArc(pen, startPoint.X, startPoint.Y, width, height, 0, 180); // example arc
+        }
+
+        private void DrawPie(Graphics g)
+        {
+            int width = Math.Abs(endPoint.X - startPoint.X);
+            int height = Math.Abs(endPoint.Y - startPoint.Y);
+            g.DrawPie(pen, startPoint.X, startPoint.Y, width, height, 0, 90); // example pie
+        }
+
+        private void DrawPolygon(Graphics g)
+        {
+            Point[] points = { startPoint, new Point(endPoint.X, startPoint.Y), endPoint, new Point(startPoint.X, endPoint.Y) };
+            g.DrawPolygon(pen, points);
+        }
+
+        private void DrawBezierCurve(Graphics g)
+        {
+            // Example Bezier curve with control points
+            Point controlPoint1 = new Point(startPoint.X, endPoint.Y);
+            Point controlPoint2 = new Point(endPoint.X, startPoint.Y);
+            g.DrawBezier(pen, startPoint, controlPoint1, controlPoint2, endPoint);
+        }
+
+        private void buttonColor_Click_1(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                pen.Color = colorDialog.Color;
+            }
+
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            canvasBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = canvasBitmap;
+            pictureBox1.Invalidate();
+
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Image backgroundImage = Image.FromFile(openFileDialog.FileName);
+                Graphics g = Graphics.FromImage(canvasBitmap);
+                g.DrawImage(backgroundImage, 0, 0, pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.Invalidate(); // Refresh the pictureBox
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG Image|*.png|JPEG Image|*.jpg|Bitmap Image|*.bmp";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                canvasBitmap.Save(saveFileDialog.FileName);
+            }
+        }
+
+        private void SaveState()
+        {
+            undoStack.Push((Bitmap)canvasBitmap.Clone());
+            redoStack.Clear();
+        }
+
+        private void Undo()
+        {
+            if (undoStack.Count > 0)
+            {
+                redoStack.Push((Bitmap)canvasBitmap.Clone());
+                canvasBitmap = undoStack.Pop();
+                pictureBox1.Image = canvasBitmap;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void Redo()
+        {
+            if (redoStack.Count > 0)
+            {
+                undoStack.Push((Bitmap)canvasBitmap.Clone());
+                canvasBitmap = redoStack.Pop();
+                pictureBox1.Image = canvasBitmap;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void UndoBtn_Click(object sender, EventArgs e)
+        {
+          Undo();
+        }
+
+        private void RedoBtn_Click(object sender, EventArgs e)
+        {
+            Redo();
+        }
+    }
+}
